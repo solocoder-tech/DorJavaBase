a49996ac1f6789b5cb40107f513ad1a86acc54a2
DESKTOP-I57GE7H\zwj <919353751@qq.com>
2020-11-19 23:19:25 +0800

    第一提交

---
+++ IODemo/src/FileDemo.java
@@ -0,0 +1,13 @@
+import java.io.File;
+
+/**
+ * whw(what,how,why):
+ * java.io.File: 文件和文件目录路径的抽象表示形式
+ * File能 新建、删除、重命名文件和目录，但是File不能访问文件内容本身，需要IO来访问
+ *
+ * 常见构造方法;
+ *
+ */
+public class FileDemo {
+    File file = new File();
+}
